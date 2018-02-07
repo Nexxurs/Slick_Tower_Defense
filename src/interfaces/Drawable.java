1369a92276d2fb1251470ae890264d2fca08e325
Nexxurs <p.uebermasser@gmx.at>
2018-02-07 15:28:23 +0100

    interfaces and test "game"

---
+++ src/interfaces/Drawable.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+import org.newdawn.slick.Graphics;
+
+
+public interface Drawable {
+    void drawMe(Graphics graphics);
+
+}
--- src/main/Game.java
+++ src/main/Game.java
@@ -4,22 +4,35 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
 
 public class Game extends BasicGame {
+
+    Rectangle rect;
 
     public Game(){
         super("Turret Defense");
     }
 
-    public void init(GameContainer gameContainer) throws SlickException {
 
+
+    public void init(GameContainer gameContainer) throws SlickException {
+        rect = new Rectangle(0,0,20,20);
     }
 
     public void update(GameContainer gameContainer, int i) throws SlickException {
+        float newX = rect.getX()+5;
+        newX = newX % gameContainer.getWidth();
+        rect.setX(newX);
 
+        float newY = rect.getY()+5;
+        newY = newY % gameContainer.getHeight();
+        rect.setY(newY);
     }
 
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+        graphics.fill(rect);
+        graphics.draw(rect);
 
     }
 }
