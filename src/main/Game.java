0a6a92eee13918ef7ce2bb100956b9f66969ec87
Nexxurs <p.uebermasser@gmx.at>
2018-02-07 14:56:56 +0100

    added libs to run on windows

---
+++ src/main/Game.java
@@ -0,0 +1,25 @@
+package main;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Game extends BasicGame {
+
+    public Game(){
+        super("Turret Defense");
+    }
+
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,21 @@
 package main;
 
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.SlickException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Test");
+    public static final int WIDTH = 600;
+    public static final int HEIGHT = 400;
+
+    public static void main(String[] args) throws SlickException {
+        BasicGame game = new Game();
+        AppGameContainer container = new AppGameContainer(game);
+        container.setDisplayMode(WIDTH, HEIGHT, false);
+        container.setVSync(true);
+        container.start();
+
+
     }
 }
